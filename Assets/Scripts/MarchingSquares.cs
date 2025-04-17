7caf47ec1200e66c05eb909ee409b968a2f01138
Tyler Baylson <thbaylson@gmail.com>
2025-04-17 04:17:59 -0400

    Lessons 3.6 and 3.7: Create Tileset and Create Level Geometry. Added dungeon assets. Created two tileset scriptable objects. Finished Marching Squares implementation for instantiating dungeon asset prefabs. Added gitattributes to handle large files.

--- Assets/Scripts/MarchingSquares.cs
+++ Assets/Scripts/MarchingSquares.cs
@@ -5,20 +5,30 @@ using UnityEngine;
 public class MarchingSquares : MonoBehaviour
 {
     [SerializeField] Texture2D levelTexture;
-    
+    [SerializeField] GameObject generatedLevel;
+    [SerializeField] Tileset tileset;
+    [SerializeField] int scale = 1;
+
     [ContextMenu("Create Level Geometry")]
     public void CreateLevelGeometry()
     {
+        generatedLevel.transform.DestroyAllChildren();
+        Vector3 scaleVector = new Vector3(scale, scale, scale);
+
         TextureBasedLevel level = new TextureBasedLevel(levelTexture);
         for (int y = 0; y < level.Length - 1; y++)
         {
-            string line = "";
             for (int x = 0; x < level.Length - 1; x++)
             {
                 int tileIndex = CalculateTileIndex(level, x, y);
-                line += tileIndex.ToString("00") + " ";
+                GameObject prefab = tileset.GetTile(tileIndex);
+                if(prefab == null) continue;
+
+                GameObject tile = Instantiate(prefab, generatedLevel.transform);
+                tile.transform.localScale = scaleVector;
+                tile.transform.position = new Vector3(x * scale, 0f, y * scale);
+                tile.name = $"{x}, {y} - {tileIndex.ToString("00")}";
             }
-            Debug.Log(line);
         }
     }
 
@@ -30,7 +40,7 @@ public class MarchingSquares : MonoBehaviour
         int topRight = level.IsBlocked(x + 1, y + 1) ? 1 : 0;
         int bottomLeft = level.IsBlocked(x, y) ? 1 : 0;
         int bottomRight = level.IsBlocked(x + 1, y) ? 1 : 0;
-        int tileIndex = topLeft + 2*topRight + 4*bottomRight + 8*bottomLeft;
+        int tileIndex = topLeft + 2*topRight + 4*bottomLeft + 8*bottomRight;
 
         return tileIndex;
     }
