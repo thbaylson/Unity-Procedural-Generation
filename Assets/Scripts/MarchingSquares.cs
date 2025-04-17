4059a1e3a0d0b4b534e602961fe8e4e5c32c85de
Tyler Baylson <thbaylson@gmail.com>
2025-04-17 03:07:50 -0400

    Lesson 3.4: Calculate Tile Index. Implemented the Marching Squares algorithm to create a 2D array of tile configurations.

--- Assets/Scripts/MarchingSquares.cs
+++ Assets/Scripts/MarchingSquares.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MarchingSquares : MonoBehaviour
 {
     [SerializeField] Texture2D levelTexture;
+    
     [ContextMenu("Create Level Geometry")]
     public void CreateLevelGeometry()
     {
@@ -14,9 +15,23 @@ public class MarchingSquares : MonoBehaviour
             string line = "";
             for (int x = 0; x < level.Length - 1; x++)
             {
-                line += level.IsBlocked(x, y) ? "#" : " ";
+                int tileIndex = CalculateTileIndex(level, x, y);
+                line += tileIndex.ToString("00") + " ";
             }
             Debug.Log(line);
         }
+    }
+
+    private int CalculateTileIndex(ILevel level, int x, int y)
+    {
+        // Calculate the tile index based on the state of the four corners
+        // TODO: Consider a less hardcoded solution.
+        int topLeft = level.IsBlocked(x, y + 1) ? 1 : 0;
+        int topRight = level.IsBlocked(x + 1, y + 1) ? 1 : 0;
+        int bottomLeft = level.IsBlocked(x, y) ? 1 : 0;
+        int bottomRight = level.IsBlocked(x + 1, y) ? 1 : 0;
+        int tileIndex = topLeft + 2*topRight + 4*bottomRight + 8*bottomLeft;
+
+        return tileIndex;
     }
 }
