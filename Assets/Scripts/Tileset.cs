932e9bc93c6860488b482e468e3cf45997e9c3fa
Tyler Baylson <thbaylson@gmail.com>
2025-07-18 15:46:37 -0400

    Fixed some formatting inconsistencies.

--- Assets/Scripts/Tileset.cs
+++ Assets/Scripts/Tileset.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 namespace ProceduralGeneration
 {
-[CreateAssetMenu(fileName = "Tileset", menuName = "ScriptableObjects/Procedural Generation/Tileset", order = 1)]
-public class Tileset : ScriptableObject
-{
-    [SerializeField] Color wallColor;
-    [SerializeField] TileVariant[] tiles = new TileVariant[16];
-
-    public Color WallColor => wallColor;
-
-    public GameObject GetTile(int tileIndex)
+    [CreateAssetMenu(fileName = "Tileset", menuName = "ScriptableObjects/Procedural Generation/Tileset", order = 1)]
+    public class Tileset : ScriptableObject
     {
-        if (tileIndex >= tiles.Length) return null;
+        [SerializeField] Color wallColor;
+        [SerializeField] TileVariant[] tiles = new TileVariant[16];
 
-        return tiles[tileIndex].GetRandomTile();
+        public Color WallColor => wallColor;
+
+        public GameObject GetTile(int tileIndex)
+        {
+            if (tileIndex >= tiles.Length) return null;
+
+            return tiles[tileIndex].GetRandomTile();
+        }
     }
-}
 }
