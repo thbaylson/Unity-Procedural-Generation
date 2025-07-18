55969f42713d078cb7a65c88da42c8492cfdc715
Tyler Baylson <thbaylson@gmail.com>
2025-07-18 15:27:47 -0400

    Fixed an issue with the namespace placement.

--- Assets/Scripts/Tileset.cs
+++ Assets/Scripts/Tileset.cs
@@ -1,22 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Tileset", menuName = "ScriptableObjects/Procedural Generation/Tileset", order = 1)]
 namespace ProceduralGeneration
 {
-public class Tileset : ScriptableObject
-{
-    [SerializeField] Color wallColor;
-    [SerializeField] TileVariant[] tiles = new TileVariant[16];
-
-    public Color WallColor => wallColor;
-
-    public GameObject GetTile(int tileIndex)
+    [CreateAssetMenu(fileName = "Tileset", menuName = "ScriptableObjects/Procedural Generation/Tileset", order = 1)]
+    public class Tileset : ScriptableObject
     {
-        if (tileIndex >= tiles.Length) return null;
+        [SerializeField] Color wallColor;
+        [SerializeField] TileVariant[] tiles = new TileVariant[16];
 
-        return tiles[tileIndex].GetRandomTile();
+        public Color WallColor => wallColor;
+
+        public GameObject GetTile(int tileIndex)
+        {
+            if (tileIndex >= tiles.Length) return null;
+
+            return tiles[tileIndex].GetRandomTile();
+        }
     }
-}
 }
